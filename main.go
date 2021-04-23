41466b553e6292d4dc595d854751b9b5cb5a8612
sanix <sanicksikani@gmail.com>
2021-04-23 16:41:50 +0300

    Get all

--- main.go
+++ main.go
@@ -3,9 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/gomodule/redigo/redis"
 )
+
+type Album struct {
+	Title  string
+	Artist string
+	Price  float64
+	Likes  int
+}
+
+func populateAlbum(reply map[string]string) (*Album, error) {
+	var err error
+	album := new(Album)
+	album.Title = reply["title"]
+	album.Artist = reply["artist"]
+
+	album.Price, err = strconv.ParseFloat(reply["price"], 64)
+	if err != nil {
+		return nil, err
+	}
+
+	album.Likes, err = strconv.Atoi(reply["likes"])
+	if err != nil {
+		return nil, err
+	}
+	return album, nil
+}
 
 func main() {
 	//establish connection
@@ -20,25 +46,23 @@ func main() {
 	/*_, err = conn.Do("HMSET", "album:2", "title", "Electric Ladyland", "artist", "Jimi Hendrix", "price", 4.95, "likes", 8)
 	if err != nil {
 		log.Fatal(err)
-	}*/
+	}
 	//get titile and converting it to string
 	title, err := redis.String(conn.Do("HGET", "album:2", "title"))
 	if err != nil {
 		log.Fatal(err)
-	}
-	artist, err := redis.String(conn.Do("HGET", "album:2", "artist"))
+	}*/
+	//HGETALL to get array of replies
+	reply, err := redis.StringMap(conn.Do("HGETALL", "album:2"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	price, err := redis.Float64(conn.Do("HGET", "album:2", "price"))
+	album, err := populateAlbum(reply)
 	if err != nil {
 		log.Fatal(err)
 	}
-	likes, err := redis.Int(conn.Do("HGET", "album:2", "likes"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s by %s: $%.2f [%d likes]\n", title, artist, price, likes)
+
+	fmt.Println(album)
 
 	//fmt.Println("Electric Ladyland added")
 
