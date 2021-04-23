0133dbed5a4789f9ebd078181f7435cbc2e82dc9
sanix <sanicksikani@gmail.com>
2021-04-23 16:31:58 +0300

    init

--- main.go
+++ main.go
@@ -2,18 +2,44 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
 
-	"github.com/gorilla/mux"
+	"github.com/gomodule/redigo/redis"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler).Methods("GET")
-	http.Handle("/", r)
-	http.ListenAndServe(":8000", nil)
-}
+	//establish connection
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+	//close the connection
+	defer conn.Close()
+
+	/*_, err = conn.Do("HMSET", "album:2", "title", "Electric Ladyland", "artist", "Jimi Hendrix", "price", 4.95, "likes", 8)
+	if err != nil {
+		log.Fatal(err)
+	}*/
+	//get titile and converting it to string
+	title, err := redis.String(conn.Do("HGET", "album:2", "title"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	artist, err := redis.String(conn.Do("HGET", "album:2", "artist"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	price, err := redis.Float64(conn.Do("HGET", "album:2", "price"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	likes, err := redis.Int(conn.Do("HGET", "album:2", "likes"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s by %s: $%.2f [%d likes]\n", title, artist, price, likes)
+
+	//fmt.Println("Electric Ladyland added")
+
 }
