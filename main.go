ebd404076211b9c18b78b094ebad74dda0181f0f
sanix <sanicksikani@gmail.com>
2021-04-23 15:24:23 +0300

    gorilla/mux

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+}
